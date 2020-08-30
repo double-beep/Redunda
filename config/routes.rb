0797c24a2a2620c07dcf39d5fb5ac7174c98e1f6
double-beep <doublebeep7@gmail.com>
2020-08-30 14:08:09 +0000

    Format routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,42 +1,40 @@
 Rails.application.routes.draw do
-  get 'bot_instances/show'
 
-  get 'users', to: 'users#index'
-
-  devise_for :users
   root to: 'bots#index'
 
-  get 'bots/data.json', to: 'bots#list_data', as: :list_data
-  get 'bots/data/:data_key', to: 'bots#get_data', as: :get_data, constraints: {:data_key => /[\w\.]+/}
-  post 'bots/data/:data_key', to: 'bots#update_data', as: :update_data, constraints: {:data_key => /[\w\.]+/}
-  delete 'bots/data/:data_key', to: 'bots#remove_data', as: :remove_data, constraints: {:data_key => /[\w\.]+/}
-  delete 'bots/:id/data/:data_key', to: 'bots#web_remove_data', as: :web_remove_data, constraints: {:data_key => /[\w\.]+/}
+  get 'users', to: 'users#index'
+  devise_for :users
 
-  post 'bots/:id/events/:name', to: 'bots#add_event', as: :add_event
-  post 'events.json', to: 'bot_instances#show_events', as: :show_events
-
+  get    'bots/data.json',                        to: 'bots#list_data',           as: :list_data
+  get    'bots/data/:data_key',                   to: 'bots#get_data',            as: :get_data, constraints: {:data_key => /[\w\.]+/}
+  get    'bots/:id',                              to: 'bots#show',                as: :bot_show
+  post   'bots/data/:data_key',                   to: 'bots#update_data',         as: :update_data, constraints: {:data_key => /[\w\.]+/}
+  post   'bots/:id/events/:name',                 to: 'bots#add_event',           as: :add_event
+  post   'bots/:bot/collaborators',               to: 'bots#add_collaborator',    as: :add_collaborator
+  delete 'bots/:bot/collaborators/:collaborator', to: 'bots#remove_collaborator', as: :remove_collaborator
+  delete 'bots/data/:data_key',                   to: 'bots#remove_data',         as: :remove_data, constraints: {:data_key => /[\w\.]+/}
+  delete 'bots/:id/data/:data_key',               to: 'bots#web_remove_data',     as: :web_remove_data, constraints: {:data_key => /[\w\.]+/}
   resources :bots do
     resources :bot_instances
   end
-  post 'bots/:bot/collaborators', to: 'bots#add_collaborator', as: :add_collaborator
-  delete 'bots/:bot/collaborators/:collaborator', to: 'bots#remove_collaborator', as: :remove_collaborator
 
-  get 'bots/:id', to: 'bots#show', as: :bot_show
-  post 'bots/:bot_id/bot_instances/reorder', to: 'bot_instances#reorder', as: :reorder
 
-  post 'bots/:bot_id/bot_instances/:id/revoke_key', to: 'bot_instances#revoke_key', as: :revoke_key
+  get 'bot_instances/show'
+  post 'bots/:bot_id/bot_instances/reorder',        to: 'bot_instances#reorder',     as: :reorder
+  post 'status.json',                               to: 'bot_instances#status_ping', as: :status_ping
+  post 'events.json',                               to: 'bot_instances#show_events', as: :show_events
+  post 'bots/:bot_id/bot_instances/:id/revoke_key', to: 'bot_instances#revoke_key',  as: :revoke_key
 
-  post 'status.json', to: 'bot_instances#status_ping', as: :status_ping
-  get 'status/code.json', to: 'code_status#api'
-  get 'status/code', as: :code_status, to: 'code_status#index'
+  get 'status/code.json',                           to: 'code_status#api'
+  get 'status/code',                                to: 'code_status#index',         as: :code_status
 
 
   scope 'authentication' do
     get 'login_redirect_target', to: 'authentication#login_redirect_target'
 
     if Rails.env.development?
-      get 'dev-login', to: 'authentication#dev_login'
-      post 'dev-login', to: 'authentication#submit_dev_login'
+      get 'dev-login',           to: 'authentication#dev_login'
+      post 'dev-login',          to: 'authentication#submit_dev_login'
     end
   end
 
